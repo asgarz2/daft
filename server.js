0c380b29c21a3d42301330b3dc2180925d28b0e7
asgarz2 <zeeshan.asgar2@mail.dcu.ie>
2019-12-04 22:13:04 +0000

    intial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('index')
+})
+
+app.post('/plots', function (req, res) {
+
+  console.log(req.body);
+
+  var city = req.body.city;
+  var bhk = req.body.bhk;
+  var price = req.body.price;
+  var trends_image_path = "images/trends/" + city + "_" + bhk + "_trend.png"
+  var forecast_image_path = "images/forecast/" + city + "_" + bhk + "_forecast.png"
+  console.log(trends_image_path);
+  console.log(forecast_image_path);
+  res.render('plot.ejs',{trends_img: trends_image_path, forecast_img: forecast_image_path});
+  // let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+// request(url, function (err, response, body) {
+//     if(err){
+//       res.render('index', {weather: null, error: 'Error, please try again'});
+//     } else {
+//       let weather = JSON.parse(body)
+//       if(weather.main == undefined){
+//         res.render('index', {weather: null, error: 'Error, please try again'});
+//       } else {
+//         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+//         res.render('index', {weather: weatherText, error: null});
+//       }
+//     }
+//   });
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
