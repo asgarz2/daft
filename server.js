87f9568f3fe41b6ee22c7095ebc33c852b56294f
asgarz2 <zeeshan.asgar2@mail.dcu.ie>
2019-12-07 21:08:51 +0000

    request added and css adjusted

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
 
 const dotenv = require('dotenv');
@@ -33,6 +34,7 @@ app.post('/plots', function (req, res) {
 
   let url = URL + `?areas=` + city + `&beds=` + bhk;
   request(url, function (err, response, body) {
+    console.log(`here`);
     if(!err){
       data = JSON.parse(body)
     }
